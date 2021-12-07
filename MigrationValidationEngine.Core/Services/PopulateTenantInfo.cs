2c1a9075439fb095e3b2b3d8d847c01c0699bc6b
Kevin McDonnell <kevin.mcdonnell@cps.co.uk>
2021-12-07 20:53:29 +0000

    Added first service

---
+++ MigrationValidationEngine.Core/Services/PopulateTenantInfo.cs
@@ -0,0 +1,13 @@
+usdotneting System;
+using Mcd79.MigrationValidationEngine.Core.Model;
+
+namespace Mcd79.MigrationValidationEngine.Core.Services
+{
+    //TODO: consider not using this and use a standard site model as staging
+    public class PopulateTenantInfo
+    {
+        public static MVETenant Retrieve (string TenantUrl) {
+
+        }
+    }
+}
